d2bd84f50f9f7bee3d2d5698d73d1f5df9e2f665
Julia Tkalych <jtkalych@Julias-MacBook-Pro.local>
2019-08-07 18:55:05 -0400

    final thing

--- Assets/app.js
+++ Assets/app.js
@@ -1,1 +1,164 @@
-console.log("file linked");
+var card = $("#quiz-area");
+var countStartNumber = 30;
+
+// Question set
+var questions = [{
+  question: "What was your favourite movie?",
+  answers: ["American History X", "Lord of the Rings", "Harry Potter", "Running With Sissors"],
+  correctAnswer: "American History X",
+  image: "https://images6.alphacoders.com/401/thumb-1920-401139.jpg"
+}, {
+  question: "Which is your favourite actor/Actress?",
+  answers: ["Ryan Gosling", "Rachel Mcadams", "Meryl Streep"],
+  correctAnswer: "Meryl Streep",
+  image: "https://www.biography.com/.image/ar_1:1%2Cc_fill%2Ccs_srgb%2Cg_face%2Cq_auto:good%2Cw_300/MTE1ODA0OTcxNzk1NjQ1OTY1/meryl-streep-9497266-1-402.jpg"
+}, {
+  question: "What is the best sports team??",
+  answers: ["Raptors", "Blue Jays", "Calgary Flames", "Edmonton Oilers"],
+  correctAnswer: "Raptors",
+  image: "https://images.rogersdigitalmedia.com/www.sportsnet.ca/team_logos/200x200/basketball/nba/toronto-raptors.png"
+}, {
+  question: "What did Britney Spears do that made everyone think she's crazy",
+  answers: ["Lip sync", "Date Justin Timberlake", "Shave her head for no reason"],
+  correctAnswer: "Shave her head for no reason",
+  image: "https://s.yimg.com/ny/api/res/1.2/3m1JkdU3Y0jPr095Bh9mCg--~A/YXBwaWQ9aGlnaGxhbmRlcjtzbT0xO3c9NzQ0O2g9ODE1/http://media.zenfs.com/en/homerun/feed_manager_auto_publish_494/8aa443b7578bfa29dbfbebca2765f00c"
+}];
+
+
+// Variable to hold our setInterval
+var timer;
+
+var game = {
+
+  questions: questions,
+  currentQuestion: 0,
+  counter: countStartNumber,
+  correct: 0,
+  incorrect: 0,
+
+  countdown: function() {
+    this.counter--;
+    $("#counter-number").text(this.counter);
+    if (this.counter === 0) {
+      console.log("TIME UP");
+      this.timeUp();
+    }
+  },
+
+  loadQuestion: function() {
+
+    timer = setInterval(this.countdown.bind(this), 1000);
+
+    card.html("<h2>" + questions[this.currentQuestion].question + "</h2>");
+
+    for (var i = 0; i < questions[this.currentQuestion].answers.length; i++) {
+      card.append("<button class='answer-button' id='button' data-name='" + questions[this.currentQuestion].answers[i]
+      + "'>" + questions[this.currentQuestion].answers[i] + "</button>");
+    }
+  },
+
+  nextQuestion: function() {
+    this.counter = window.countStartNumber;
+    $("#counter-number").text(this.counter);
+    this.currentQuestion++;
+    this.loadQuestion.bind(this)();
+  },
+
+  timeUp: function() {
+
+    clearInterval(window.timer);
+
+    $("#counter-number").text(this.counter);
+
+    card.html("<h2>Out of Time!</h2>");
+    card.append("<h3>The Correct Answer was: " + questions[this.currentQuestion].correctAnswer);
+    card.append("<img src='" + questions[this.currentQuestion].image + "' />");
+
+    if (this.currentQuestion === questions.length - 1) {
+      setTimeout(this.results, 3 * 1000);
+    }
+    else {
+      setTimeout(this.nextQuestion, 3 * 1000);
+    }
+  },
+
+  results: function() {
+
+    clearInterval(window.timer);
+
+    card.html("<h2>All done, heres how you did!</h2>");
+
+    $("#counter-number").text(this.counter);
+
+    card.append("<h3>Correct Answers: " + this.correct + "</h3>");
+    card.append("<h3>Incorrect Answers: " + this.incorrect + "</h3>");
+    card.append("<h3>Unanswered: " + (questions.length - (this.incorrect + this.correct)) + "</h3>");
+    card.append("<br><button id='start-over'>Start Over?</button>");
+  },
+
+  clicked: function(e) {
+    clearInterval(window.timer);
+    if ($(e.target).attr("data-name") === questions[this.currentQuestion].correctAnswer) {
+      this.answeredCorrectly();
+    }
+    else {
+      this.answeredIncorrectly();
+    }
+  },
+
+  answeredIncorrectly: function() {
+
+    this.incorrect++;
+
+    clearInterval(window.timer);
+
+    card.html("<h2>Nope!</h2>");
+    card.append("<h3>The Correct Answer was: " + questions[this.currentQuestion].correctAnswer + "</h3>");
+    card.append("<img src='" + questions[this.currentQuestion].image + "' />");
+
+    if (this.currentQuestion === questions.length - 1) {
+      setTimeout(this.results.bind(this), 3 * 1000);
+    }
+    else {
+      setTimeout(this.nextQuestion.bind(this), 3 * 1000);
+    }
+  },
+
+  answeredCorrectly: function() {
+
+    clearInterval(window.timer);
+
+    this.correct++;
+
+    card.html("<h2>Correct!</h2>");
+    card.append("<img src='" + questions[this.currentQuestion].image + "' />");
+
+    if (this.currentQuestion === questions.length - 1) {
+      setTimeout(this.results.bind(this), 3 * 1000);
+    }
+    else {
+      setTimeout(this.nextQuestion.bind(this), 3 * 1000);
+    }
+  },
+
+  reset: function() {
+    this.currentQuestion = 0;
+    this.counter = countStartNumber;
+    this.correct = 0;
+    this.incorrect = 0;
+    this.loadQuestion();
+  }
+};
+
+// CLICK EVENTS
+
+$(document).on("click", "#start-over", game.reset.bind(game));
+
+$(document).on("click", ".answer-button", function(e) {
+  game.clicked.bind(game, e)();
+});
+
+$(document).on("click", "#start", function() {
+  $("#sub-wrapper").prepend("<h2>Time Remaining: <span id='counter-number'>30</span> Seconds</h2>");
+  game.loadQuestion.bind(game)();
+});
